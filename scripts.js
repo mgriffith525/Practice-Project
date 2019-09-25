30d597cac49f910979881400b908c7e41c388820
Meghan Griffith <meghan.griffith@bonfyreapp.com>
2019-09-24 20:17:42 -0500

    Add root div.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function greeter (question = "What is your name?") {
   const answer = prompt(question);
 
   if (answer === "") {
-    greeter(); // recursion
+    return greeter(); // recursion
   }
   return answer;
 }
