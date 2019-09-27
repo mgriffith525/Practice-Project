8c70ac8e80d6fb21ea58bbbf13a1473947b96d4f
Meghan Griffith <meghan.griffith@bonfyreapp.com>
2019-09-26 20:46:39 -0500

    Add all initial components and parcel.

---
+++ components/Main.js
@@ -0,0 +1,31 @@
+export default `<main>
+<div class="hero">
+  <a href="#">Fake CTA Button</a>
+</div>
+
+<div class="flex-container--desktop">
+<section>
+  <h2>Some Fake Heading</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nam tempore ducimus maxime mollitia omnis recusandae facere unde dignissimos laborum tenetur itaque, quasi dolorum magnam placeat ipsa ratione non dolore?
+  </p>
+  <a href="" class="read-more-btn">Read More!</a>
+</section>
+
+<section>
+  <h2>Some Fake Heading</h2>
+  <p>
+    Reiciendis deleniti blanditiis quod fuga impedit illo error saepe repellendus eos nisi ratione, cumque obcaecati dignissimos aliquid iure quos cupiditate unde provident! Exercitationem minima obcaecati minus perferendis quasi fuga accusamus?
+  </p>
+  <a href="" class="read-more-btn">Read More!</a>
+</section>
+
+<section>
+  <h2>Some Fake Heading</h2>
+  <p>
+    Doloribus alias expedita officia molestias? Corporis ab dolore molestiae enim voluptatum maiores debitis explicabo distinctio eum aliquam quidem temporibus a harum, repellendus, nam dolorem odio similique esse expedita omnis quas.
+  </p>
+  <a href="" class="read-more-btn">Read More!</a>
+</section>
+</div>
+</main>`
